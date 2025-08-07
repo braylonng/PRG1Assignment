ad1686cd938e113dbd36efdcc37268aa8ec52c4c
braylonng <s10272373@connect.np.edu.sg>
2025-08-07 23:41:04 +0800

    6th commit

--- Sundrop_Caves_template.py
+++ Sundrop_Caves_template.py
@@ -1,6 +1,7 @@
 from random import randint
 import os
-
+current_level =1
+max_level=2
 original_ore_positions = []
 player = {}
 game_map = []
@@ -10,7 +11,7 @@ MAP_WIDTH = 0
 MAP_HEIGHT = 0
 
 TURNS_PER_DAY = 20
-WIN_GP = 500
+WIN_GP = 750 #increased from 500, due to the addition of another level
 
 minerals = ['copper', 'silver', 'gold']
 mineral_names = {'C': 'copper', 'S': 'silver', 'G': 'gold'}
@@ -23,6 +24,7 @@ prices['gold'] = (10, 18)
 
 # This function loads a map structure (a nested list) from a file
 # It also updates MAP_WIDTH and MAP_HEIGHT
+
 def load_map(filename, map_struct):
     global MAP_WIDTH, MAP_HEIGHT
     map_struct.clear()
@@ -52,10 +54,10 @@ def clear_fog(fog, player):
                 fog[y][x] = False
     return
 
-def initialize_game(game_map, fog, player):
+def initialize_game(game_map, fog, player, level_file='level1.txt', level=1):
     global MAP_WIDTH, MAP_HEIGHT, original_ore_positions
     # initialize map
-    load_map("level1.txt", game_map)
+    load_map(level_file, game_map)
     fog.clear()
     original_ore_positions.clear()
     for _ in range(MAP_HEIGHT):
@@ -64,6 +66,8 @@ def initialize_game(game_map, fog, player):
     # TODO: initialize player
     player.clear()
     #   You will probably add other entries into the player dictionary
+    player['name'] = name
+    player['level'] = level
     player['torch'] = False
     player['portal'] = (0,0)
     player['pickaxe']=1
@@ -140,7 +144,7 @@ def show_information(player):
     print(f"Load: {player['load']} / {player['max_load']}")
     print(f"GP: {player['GP']}")
     print(f"Steps taken: {player['steps']}")
-    #print(f"Warehouse: {player['warehouse']}")
+    print(f"Warehouse: {player['warehouse']}")
     print("------------------------------\n")
     return
 
@@ -159,13 +163,20 @@ def save_game(game_map, fog, player):
 def load_game(game_map, fog, player):
     # load map
     if not os.path.exists("savegame.txt"):
-        print("No saved game found. Please save a game before trying to load.")
+        print('No saved game found.')
         return
 
     with open("savegame.txt", "r") as savefile:
-        game_map_data = eval(savefile.readline())
-        fog_data = eval(savefile.readline())  
-        player_data = eval(savefile.readline())
+        lines = savefile.readlines()
+    if len(lines)<1:
+        print('No save file was found')
+        return
+    if any(line.strip() == "" for line in lines[:1]):
+        print("Save file contains missing data.")
+        return
+    game_map_data = eval(lines[0].strip())
+    fog_data = eval(lines[1].strip())
+    player_data = eval(lines[2].strip())
     # load map
     game_map.clear()
     game_map.extend(game_map_data)
@@ -175,6 +186,7 @@ def load_game(game_map, fog, player):
     # load player
     player.clear()
     player.update(player_data)
+
     return
 
 def show_main_menu():
@@ -182,23 +194,23 @@ def show_main_menu():
     print("--- Main Menu ----")
     print("(N)ew game")
     print("(L)oad saved game")
-#    print("(H)igh scores")
+    print("(H)igh scores")
     print("(Q)uit")
     print("------------------")
 def show_town_menu(player):
     print()
-    
-    print(f'DAY {int(player['day'])+1}')
+    print(f"DAY {int(player['day']) + 1}")
     print("----- Sundrop Town -----")
     print("(B)uy stuff")
     print("See Player (I)nformation")
     print("See Mine (M)ap")
-    print("(E)nter mine")
+    print(f"(E)nter mine. You're currently in Level {player.get('level', 1)} ")
     print("Sa(V)e game")
     print('(S)ell ores')
     print("(W)arehouse")
     print("(Q)uit to main menu")
     print("------------------------")
+game_state = 'main'
 def sell_ore():
     total =0 
     for ore in minerals:
@@ -209,9 +221,12 @@ def sell_ore():
             player ['GP'] += gp 
             player[ore] =0
             player['load']=0
-            total +=gp
+            total += gp
     if player['GP']>= WIN_GP:
         print(f"\nWoo-hoo! Well done, {player['name']}, you have {player['GP']} GP!\nYou now have enough to retire and play video games every day.\nAnd it only took you {player['day']} days and {player['steps']} steps! You win!")
+        return True
+    return False
+        
 def replenish_nodes(game_map):
     for x, y, ore_char in original_ore_positions:
         if game_map[y][x] == ' ':
@@ -273,7 +288,7 @@ def show_warehouse_menu(player):
             
 
 #--------------------------- MAIN GAME ---------------------------
-game_state = 'main'
+# game_state = 'main'
 print("---------------- Welcome to Sundrop Caves! ----------------")
 print("You spent all your money to get the deed to a mine, a small")
 print("  backpack, a simple pickaxe and a magical portal stone.")
@@ -298,9 +313,14 @@ while True:
             game_state='town'
             
         elif option.upper()=='L':
-            load_game(game_map, fog, player)
-            print("Game loaded.")
-            game_state = 'town'
+            f = open('savegame.txt').readlines()
+            if len(f) ==0:
+                print('No save file was found')
+                game_state = 'main'
+            else:
+                load_game(game_map, fog, player)
+                print("Game loaded.")
+                game_state = 'town'
         else:
             print('Invalid Option. Please try again.')
             continue
@@ -312,6 +332,9 @@ while True:
             confirm=input(f'You have {player['load']} / {player['max_load']} ores. Press S again to confirm selling.').strip()
             if confirm.upper() == 'S':
                 sell_ore()
+                if sell_ore():
+                    game_state = 'main'
+                    continue
 
         elif choice.upper()=='Q':
              game_state='main'
@@ -422,6 +445,42 @@ while True:
                     player['load']+=picked
                     game_map[new_y][new_x] = ' '
                     print(f'You mined {picked} piece(s) of {mineral}.')
+                elif tile == 'D':
+                    print('You found an old and Mysterious door, and you enter into another mine...')
+                    gp = player['GP']
+                    ores = {ore: player[ore] for ore in minerals}
+                    steps = player['steps']
+                    days = player['day']
+                    warehouse = player['warehouse']
+                    initialize_game(game_map, fog, player, "level2.txt", level=2)
+                    player['GP'] = gp
+                    for ore in minerals:
+                        player[ore] = ores[ore]
+                    player['steps'] = steps
+                    player['day'] = days
+                    player['warehouse'] = warehouse
+                    continue
+                elif tile == 'U':
+                    print("You found a stairwell leading back to the first level...")
+
+                    # Save current state
+                    gp = player['GP']
+                    ores = {ore: player[ore] for ore in minerals}
+                    steps = player['steps']
+                    days = player['day']
+                    warehouse = player['warehouse']
+                        # Load level 1
+                    initialize_game(game_map, fog, player, "level1.txt", level=1)
+
+                     # Restore player state
+                    player['GP'] = gp
+                    for ore in minerals:
+                        player[ore] = ores[ore]
+                    player['steps'] = steps
+                    player['day'] = days
+                    player['warehouse'] = warehouse
+
+                    continue
                 player['x']=new_x
                 player['y']=new_y
                 player['turns'] -= 1
@@ -453,8 +512,4 @@ while True:
         elif move == 'q':
             game_state= 'main'
         else:
-            print('Invalid option. Please try again')         
-
-
-
-                        
+            print('Invalid option. Please try again')                          
